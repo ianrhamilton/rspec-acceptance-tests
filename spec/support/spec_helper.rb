52fc25d75fa3249eb890bd45556e0908aa8a0920
ianhamilton <ian.hamilton@digital.hmrc.gov.uk>
2015-09-22 11:19:04 +0100

    --initial commit of rspec examples

--- spec/support/spec_helper.rb
+++ spec/support/spec_helper.rb
@@ -5,6 +5,9 @@ require 'allure-rspec'
 require 'pathname'
 require 'uuid'
 require 'capybara/rspec'
+require 'parallel_tests'
+require 'pry'
+require 'phantomjs'
 require_relative 'site'
 require_relative '../../lib/pages/base_page'
 require_relative '../../lib/pages/login_page'
@@ -20,29 +23,28 @@ RSpec.configure do |config|
     @driver = browser
     @driver.manage.delete_all_cookies
     @site = Site.new(@driver)
-  end
-  config.after(:each) do |example|
-    if example.exception
-      example.attach_file('screenshot', File.new(
-          @driver.save_screenshot(
-              File.join(Dir.pwd, "report/#{UUID.new.generate}.png"))))
-    end
+
   end
   config.after(:all) { @driver.close unless @driver.nil? }
 end
 
-AllureRSpec.configure do |config|
-  config.output_dir = 'report'
-  config.clean_dir = false
+AllureRSpec.configure do |c|
+  c.output_dir = "log/screenshots"
+  c.clean_dir = false
 end
 
+ParallelTests.first_process? ? FileUtils.rm_rf(AllureRSpec::Config.output_dir) : sleep(1)
+
 def browser
-  case ENV['browser']
+  case ENV['DRIVER']
     when 'firefox'
       Selenium::WebDriver.for :firefox
     when 'chrome'
-      Selenium::WebDriver::Chrome::Service.executable_path = File.join(Dir.pwd, 'vendor/chromedriver_mac')
+      Selenium::WebDriver::Chrome::Service.executable_path = File.join(Dir.pwd, 'vendor/chrome/chromedriver_mac')
       Selenium::WebDriver.for :chrome
+    when 'phantomjs'
+      # Selenium::WebDriver::PhantomJS.path = File.join(Dir.pwd, 'vendor/phantom/phantomjs')
+      Selenium::WebDriver.for :phantomjs
     else
       Selenium::WebDriver.for :firefox
   end
